69377dcfc8792e75ece57b79d0ed61b2bcb82b2e
Alexander Sesorov <samws@mail.ru>
2020-04-01 13:58:26 +0300

    Added comments

--- inline_handle.py
+++ inline_handle.py
@@ -10,6 +10,7 @@ from selenium import webdriver
 from setup import PROXY, TOKEN
 from webdriver_manager.chrome import ChromeDriverManager
 
+"""Buttons' identifiers for keyboard callback data"""
 CALLBACK_BUTTON_01 = "callback_increase_01"
 CALLBACK_BUTTON_05 = "callback_increase_05"
 CALLBACK_BUTTON_m01 = "callback_decrease_01"
@@ -24,112 +25,104 @@ bot = Bot(
 )
 
 
-class InlineKeyboard():
+class InlineKeyboardFactory():  # provides all inline keyboards
     @staticmethod
-    def get_inline_contrast_keyboard():
+    def get_inline_contrast_keyboard():  # keyboard for image contrast level
         """Get custom inline keyboard for modifying contrast of an image"""
         keyboard = [
             [
-                InlineKeyboardButton("+0.1", callback_data=CALLBACK_BUTTON_01),
-                InlineKeyboardButton("+0.5", callback_data=CALLBACK_BUTTON_05)
+                InlineKeyboardButton("+0.1", callback_data=CALLBACK_BUTTON_01),  # increases contrast by 0.1
+                InlineKeyboardButton("+0.5", callback_data=CALLBACK_BUTTON_05)  # increases contrast by 0.5
             ],
             [
-                InlineKeyboardButton("-0.1", callback_data=CALLBACK_BUTTON_m01),
-                InlineKeyboardButton("-0.5", callback_data=CALLBACK_BUTTON_m05)
+                InlineKeyboardButton("-0.1", callback_data=CALLBACK_BUTTON_m01),  # decreases contrast by 0.1
+                InlineKeyboardButton("-0.5", callback_data=CALLBACK_BUTTON_m05)  # decreases contrast by 0.5
             ],
             [
-                InlineKeyboardButton("PERFECT!", callback_data=CALLBACK_BUTTON_FIN)
+                InlineKeyboardButton("PERFECT!", callback_data=CALLBACK_BUTTON_FIN)  # finishes the editing of contrast
             ]
         ]
         return InlineKeyboardMarkup(keyboard)
 
     @staticmethod
-    def get_inline_coronavirus_keyboard():
+    def get_inline_coronavirus_keyboard():  # keyboard for /corona_stat
         """Get custom inline keyboard for coronavirus stats"""
         keyboard = [
             [
-                InlineKeyboardButton("Russia stats", callback_data=CALLBACK_BUTTON_COVID19_RU)
+                InlineKeyboardButton("Russian stats", callback_data=CALLBACK_BUTTON_COVID19_RU)  # Get Russian stats
             ]
         ]
         return InlineKeyboardMarkup(keyboard)
 
 
-class InlineCallback():
+class InlineCallback():  # Processes the events on inline keyboards' buttons
     @staticmethod
-    def handle_keyboard_callback(update: Update, context: CallbackContext):
-        query = update.callback_query
-        data = query.data
-        chat_id = update.effective_message.chat_id
+    def handle_keyboard_callback(update: Update, context: CallbackContext):  # Gets callback_data from the pushed button
+        query = update.callback_query  # Gets query from callback
+        data = query.data  # callback_data of pushed button
+        chat_id = update.effective_message.chat_id  # chat id for sending messages
 
         if data == CALLBACK_BUTTON_01:
-            img_h.get_contrast_img(0.1, 'initial.jpg', 'initial.jpg')
+            img_h.get_contrast_img(0.1, 'initial.jpg', 'initial.jpg')  # replace the existing image with an enhanced one
             temp_message = bot.send_photo(chat_id=chat_id, photo=open('initial.jpg', mode='rb'),
-                                          reply_markup=InlineKeyboard.get_inline_contrast_keyboard())
-            bot.delete_message(chat_id, temp_message.message_id - 1)
+                                          reply_markup=InlineKeyboardFactory.get_inline_contrast_keyboard())
+            bot.delete_message(chat_id, temp_message.message_id - 1)  # deletes previous message with an old image
 
         elif data == CALLBACK_BUTTON_05:
-            img_h.get_contrast_img(0.5, 'initial.jpg', 'initial.jpg')
+            img_h.get_contrast_img(0.5, 'initial.jpg', 'initial.jpg')  # replace the existing image with an enhanced one
             temp_message = bot.send_photo(chat_id=chat_id, photo=open('initial.jpg', mode='rb'),
-                                          reply_markup=InlineKeyboard.get_inline_contrast_keyboard())
-            bot.delete_message(chat_id, temp_message.message_id - 1)
+                                          reply_markup=InlineKeyboardFactory.get_inline_contrast_keyboard())
+            bot.delete_message(chat_id, temp_message.message_id - 1)  # deletes previous message with an old image
         elif data == CALLBACK_BUTTON_m01:
-            img_h.get_contrast_img(-0.1, 'initial.jpg', 'initial.jpg')
+            img_h.get_contrast_img(-0.1, 'initial.jpg', 'initial.jpg')  # replace existing image with an enhanced one
             temp_message = bot.send_photo(chat_id=chat_id, photo=open('initial.jpg', mode='rb'),
-                                          reply_markup=InlineKeyboard.get_inline_contrast_keyboard())
-            bot.delete_message(chat_id, temp_message.message_id - 1)
+                                          reply_markup=InlineKeyboardFactory.get_inline_contrast_keyboard())
+            bot.delete_message(chat_id, temp_message.message_id - 1)  # deletes previous message with an old image
         elif data == CALLBACK_BUTTON_m05:
-            img_h.get_contrast_img(-0.5, 'initial.jpg', 'initial.jpg')
+            img_h.get_contrast_img(-0.5, 'initial.jpg', 'initial.jpg')  # replace existing image with an enhanced one
             temp_message = bot.send_photo(chat_id=chat_id, photo=open('initial.jpg', mode='rb'),
-                                          reply_markup=InlineKeyboard.get_inline_contrast_keyboard())
-            bot.delete_message(chat_id, temp_message.message_id - 1)
+                                          reply_markup=InlineKeyboardFactory.get_inline_contrast_keyboard())
+            bot.delete_message(chat_id, temp_message.message_id - 1)  # deletes previous message with an old image
         elif data == CALLBACK_BUTTON_FIN:
-            img_h.get_contrast_img(1.0, 'initial.jpg', 'res.jpg')
+            img_h.get_contrast_img(1.0, 'initial.jpg', 'res.jpg')  # get final result after editing
             final_message = bot.send_photo(chat_id=chat_id, photo=open("res.jpg", mode='rb'))
-            bot.delete_message(chat_id, final_message.message_id - 1)
+            bot.delete_message(chat_id, final_message.message_id - 1)  # deletes previous message with an old image
 
         elif data == CALLBACK_BUTTON_COVID19_RU:
-            html = requests.get('https://virus-zone.ru/coronavirus-v-rossii/').text
-            driver = webdriver.Chrome(ChromeDriverManager().install())
-            driver.get('https://virus-zone.ru/coronavirus-v-rossii/')
-            table = driver.find_element_by_xpath('//div[3]/div[4]/div/table')
-            table_html = table.get_attribute('innerHTML')
-            with open('data.html', 'w+') as handle:
-                handle.write(table_html)
-            table_html = BeautifulSoup(table_html, 'html.parser')
+            driver = webdriver.Chrome(ChromeDriverManager().install())  # installs the web driver to run JS-tables on the website
+            driver.get('https://virus-zone.ru/coronavirus-v-rossii/')  # runs the page with covid-19 data for Russia
+            table = driver.find_element_by_xpath('//div[3]/div[4]/div/table')  # find the JS-generated table with statistics on covid-19 divided by regions
+            table_html = table.get_attribute('innerHTML')  # get the html-version of JS-generated table for parsing it
+            table_html = BeautifulSoup(table_html, 'html.parser')  # format it with BeautifulSoup
 
-            headings = []
-            for table_row in table_html.findAll('thead'):
-                columns = table_row.findAll('th')
-                heading = []
-                for column in columns:
-                    heading.append(column.text.strip())
-                headings.append(heading)
+            headings = []  # USELESS IN CODE, NEEDED JUST FOR BETTER UNDERSTANDING: headings of a table
+            for table_row in table_html.findAll('thead'):  # find html-tag for heading of a table
+                columns = table_row.findAll('th')  # find all components of an html-heading by their tag <th> </th>
+                heading = []  # stores single heading
+                for column in columns:  # for each column's heading:
+                    heading.append(column.text.strip())  # add heading without whitespaces (raw data provides heading
+                    # with a number of useless whitespaces, thus we need to get only letters)
+                headings.append(heading)  # add formatted heading
 
-            output_rows = []
-            for table_row in table_html.findAll('tr'):
-                columns = table_row.findAll('td')
-                output_row = []
-                for column in columns:
-                    output_row.append(column.text.strip())
-                output_rows.append(output_row)
+            output_rows = []  # ALL rows of a table
+            for table_row in table_html.findAll('tr'):  # find html-tag for row of a table
+                columns = table_row.findAll('td')  # find each cell of a row
+                output_row = []  # SINGLE row
+                for column in columns:  # for each cell:
+                    output_row.append(column.text.strip())  # add cell to the row without whitespaces
+                output_rows.append(output_row)  # add formatted row and go to the next one
 
-            with open('covid_ru.csv', 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerows(headings)
-                writer.writerows(output_rows)
+            with open('covid_ru.csv', 'w', newline='') as csvfile:  # open .csv file for storing our covid-19 RU data
+                writer = csv.writer(csvfile)  # csv writer for this file
+                writer.writerows(headings)  # firstly, add headings
+                writer.writerows(output_rows)  # then add all rows from table
 
-            with open('covid_ru.csv', 'r') as handle:
-                reader = handle.readlines()[2:7]
-                place = 0
+            with open('covid_ru.csv', 'r') as handle:  # open .csv file to get our covid-19 RU data
+                reader = handle.readlines()[2:7]  # ignore heading and 1 empty line
+                place = 0  # counter for the top-5
                 for row in reader:
-                    if place == 0:
-                        place += 1
-                        pass
-                    if any(row):
-                        content = row.split(',')
+                    place += 1
+                    if any(row):  # if the line is not empty
+                        content = row.split(',')  # split the row by ',' symbol
                         bot.send_message(chat_id=chat_id, text=f'<b>{place}.</b> {content[0]}: {content[1]} cases\n',
-                                     parse_mode=telegram.ParseMode.HTML)
-                        place += 1
-                    if place == 6:
-                        break
-                        
+                                         parse_mode=telegram.ParseMode.HTML)  # content[0] - city name, content[1] - number of cases
