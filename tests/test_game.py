261ed9b6ebc2ae2dc80dc1d4423021255ce254f6
Alexander Sesorov <samws@mail.ru>
2020-04-13 21:55:00 +0300

    Add files via upload

---
+++ tests/test_game.py
@@ -0,0 +1,61 @@
+import unittest
+
+from card import Card, GREEN
+from deck import Deck
+from game import Game
+from player import Player
+from errors import EmptyDeckError
+
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
+    def tearDown(self):
+        self.game = Game()
+
+    def test_add_player(self):
+        self.player = Player(game=self.game, is_human=True, name="John Doe")
+        self.game.add_player(player=self.player)
+        self.assertIn(self.player, self.game.players)
+
+    def test_is_player(self):
+        self.player = Player(game=self.game, is_human=True, name="John Doe")
+        self.game.add_player(player=self.player)
+        self.assertIsInstance(self.game.current_player, Player)
+
+    def test_next_turn(self):
+        self.player_1 = Player(game=self.game, is_human=True, name="John Doe")
+        self.player_2 = Player(game=self.game, is_human=False, name="Bot 1")
+        self.player_3 = Player(game=self.game, is_human=False, name="Bot 2")
+        self.game.add_player(player=self.player_1)
+        self.game.add_player(player=self.player_2)
+        self.game.add_player(player=self.player_3)
+        for i in range(5):
+            self.game.next_turn()
+        self.assertEqual(self.game.current_player, self.player_3)
+
+    def test_skip_next(self):
+        self.player_1 = Player(game=self.game, is_human=True, name="John Doe")
+        self.player_2 = Player(game=self.game, is_human=False, name="Bot 1")
+        self.player_3 = Player(game=self.game, is_human=False, name="Bot 2")
+        self.game.add_player(player=self.player_1)
+        self.game.add_player(player=self.player_2)
+        self.game.add_player(player=self.player_3)
+        self.game.skip_next()
+        self.assertEqual(self.game.current_player, self.player_3)
+
+    def test_play_card(self):
+        self.player_1 = Player(game=self.game, is_human=True, name="John Doe")
+        self.player_2 = Player(game=self.game, is_human=False, name="Bot 1")
+        self.game.add_player(player=self.player_1)
+        self.game.add_player(player=self.player_2)
+        self.last = self.game.last_card
+        self.game.play_card(self.player_1.cards[0])
+        self.assertIn(self.last, self.game.deck.discard_pile)
+
+    def test_choose_color(self):
+        self.player_1 = Player(game=self.game, is_human=True, name="John Doe")
+        self.game.add_player(player=self.player_1)
+        self.game.choose_color(GREEN)
+        self.assertIs(self.game.last_card.color, GREEN)
