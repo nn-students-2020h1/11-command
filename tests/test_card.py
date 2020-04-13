4ad7fc95eb43294030df302de4d1537da3b53d82
Alexander Sesorov <samws@mail.ru>
2020-04-13 21:55:39 +0300

    Add files via upload

---
+++ tests/test_card.py
@@ -0,0 +1,20 @@
+import unittest
+
+from card import Card, RED, ONE, DRAW_FOUR
+
+class TestCard(unittest.TestCase):
+    def setUp(self):
+        self.card = Card(None, None)
+
+    def tearDown(self):
+        self.card.color = None
+        self.card.value = None
+        self.card.special = None
+
+    def test_reveal_common(self):
+        self.card = Card(RED, ONE)
+        self.assertEqual(self.card.reveal(), [RED, ONE])
+
+    def test_reveal_special(self):
+        self.card.special = DRAW_FOUR
+        self.assertEqual(self.card.reveal(), DRAW_FOUR)
