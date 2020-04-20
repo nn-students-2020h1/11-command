bbf252184837d8eb516cc1a760688a95162f1202
DmitryTrifonov <dim.trif1234@gmail.com>
2020-04-20 14:53:54 +0300

    added new tests for covid_stat

--- unit-tests/test_covid_world_stat.py
+++ unit-tests/test_covid_world_stat.py
@@ -8,6 +8,12 @@ from pandas import DataFrame
 
 class TestCovidWorldStat(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.test_df = DataFrame({"Confirmed": [1900, 500, 1, 10000],
+                                  "Province_State": ["France", "Moscow", "Dublin", "Paris"],
+                                  "Lat": [48.85341, 55.7522, 53.3331,  48.8534],
+                                  "Long_": [2.3488, 37.6156, -6.24889, 2.3488]})
+
     def test_make_corona_stat_request(self):
         with patch('covid_stat.CovidWorldStat.make_corona_stat_request') as mock_covid_request:
             mock_covid_request.return_value = 'is not  empty'
@@ -33,3 +39,16 @@ class TestCovidWorldStat(unittest.TestCase):
         with patch('covid_stat.CovidWorldStat.get_yesterday_df') as mock_yesterday_df:
             mock_yesterday_df.return_value = DataFrame(columns=['one', 'two'])
             self.assertIsInstance(CovidWorldStat.get_yesterday_df(CovidWorldStat), DataFrame)
+
+    @patch('covid_stat.CovidWorldStat.get_difference_disease', return_value=list(range(5)))
+    def test_mock_get_differ(self, mock_differ_disease):
+        self.assertEqual(len(mock_differ_disease()), 5)
+
+    def test_make_corona_request_status_code(self):
+        with patch('covid_stat.CovidWorldStat.make_corona_stat_request') as web_page:
+            web_page.return_value.status_code.return_value = 200
+        self.assertEqual(CovidWorldStat.make_corona_stat_request().status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
