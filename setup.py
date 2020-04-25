eba6aba1807eedd6a4465dce17ca0d149b1b222f
DmitryTrifonov <dim.trif1234@gmail.com>
2020-04-25 13:41:39 +0300

    changed proxy

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 TOKEN = '1114504091:AAHuB_E7EKQIA6ysmfrlWKDX_EQ8NbetGuc'  # insert your token here
 # get socks proxy on https://webanetlabs.net/publ/9-1-0-730
-PROXY = 'socks5://92.223.93.212:1080'  # old 'https://telegg.ru/orig/bot' proxy to connect against telegram ban
+PROXY = 'https://telegg.ru/orig/bot'  # old 'https://telegg.ru/orig/bot' proxy to connect against telegram ban
