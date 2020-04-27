84094f551854870b16ef4f8e60566c4c0660a43c
sesorov <samws@mail.ru>
2020-04-27 20:10:08 +0300

    Added more tests for: - getting csv from Database - adding csv to Database

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,12 +1,9 @@
 import unittest
-import telegram
-from unittest import mock
 from unittest.mock import patch
 import auxiliary_functions
 import pymongo
 import mongomock
-import user_history
-import os
+import pandas as pd
 
 
 def get_action():
@@ -48,3 +45,25 @@ class TestCSV(unittest.TestCase):
         with patch("auxiliary_functions.db_covid_csv",
                    new=pymongo.MongoClient('testserver.com')['covid_csv']) as mock_db:
             self.assertFalse(auxiliary_functions.check_exist_dates('01.01.01'))
+
+    @mongomock.patch(servers=(('testserver.com', 27017),))
+    def test_csv_from_db(self):
+        with patch("auxiliary_functions.db_covid_csv",
+                   new=pymongo.MongoClient('testserver.com')['covid_csv']) as mock_db:
+            mock_db['01.01.01'].insert_one({"test": "test"})
+            self.assertEqual(auxiliary_functions.get_csv_from_db('01.01.01'), {"test": "test"})
+
+    @mongomock.patch(servers=(('testserver.com', 27017),))
+    def test_add_csv_to_db(self):
+        with patch("auxiliary_functions.db_covid_csv",
+                   new=pymongo.MongoClient('testserver.com')['covid_csv']) as mock_db:
+            with patch('auxiliary_functions.pandas.read_csv') as mock_read_csv:
+                test_data = [['45001', 'Abbeville', 'South Carolina', 'US']]
+                test_dataframe = pd.DataFrame(test_data, columns=['FIPS', 'Admin2', 'Province_State', 'Country_Region'])
+                mock_read_csv.return_value = test_dataframe
+                auxiliary_functions.add_date_to_db('01.01.01')
+                self.assertEqual(auxiliary_functions.get_csv_from_db('01.01.01'), {'Admin2': {'0': 'Abbeville'},
+                                                                                   'Country_Region': {'0': 'US'},
+                                                                                   'FIPS': {'0': '45001'},
+                                                                                   'Province_State': {
+                                                                                       '0': 'South Carolina'}})
